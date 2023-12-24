01d43d0484992db19255b9918d9d0dbf75829ed1
ssubudhi <ssubudhi@sashsys.com>
2023-12-25 00:05:41 +0530

    Episode-01 Changes

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ *   <div id="parent">
+ *       <div id="child">
+ *           <h2>I am h2 an tag</h2>
+ *           <h1>I am h1 an tag</h1>
+ *       </div>
+ *   </div> 
+ *   <div id="parent">
+ *       <div id="child">
+ *           <h2>I am h2 an tag</h2>
+ *           <h1>I am h1 an tag</h1>
+ *       </div>
+ *   </div>
+*/
+
+const parent = React.createElement("div",{ id: "parent" },
+    [React.createElement("div",{ id: "child" },
+        [
+            React.createElement( "h1",{},"I am an h1 tag"),
+            React.createElement( "h2",{},"I am an h2 tag")
+        ]
+    ),
+    React.createElement("div",{ id: "child" },
+        [
+            React.createElement( "h1",{},"I am an h1 tag"),
+            React.createElement( "h2",{},"I am an h2 tag")
+        ]
+    )]
+);
+
+//JSX (to make above work simpler)
+
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello World using React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); 
