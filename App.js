f03cc95488ab4b05d96fb70f5edf4ed96eb57405
ssubudhi <ssubudhi@sashsys.com>
2024-06-10 16:35:38 +0530

    Episode 2 & 3 Changes

--- App.js
+++ App.js
@@ -1,41 +1,6 @@
-/**
- * 
- *   <div id="parent">
- *       <div id="child">
- *           <h2>I am h2 an tag</h2>
- *           <h1>I am h1 an tag</h1>
- *       </div>
- *   </div> 
- *   <div id="parent">
- *       <div id="child">
- *           <h2>I am h2 an tag</h2>
- *           <h1>I am h1 an tag</h1>
- *       </div>
- *   </div>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{ id: "parent" },
-    [React.createElement("div",{ id: "child" },
-        [
-            React.createElement( "h1",{},"I am an h1 tag"),
-            React.createElement( "h2",{},"I am an h2 tag")
-        ]
-    ),
-    React.createElement("div",{ id: "child" },
-        [
-            React.createElement( "h1",{},"I am an h1 tag"),
-            React.createElement( "h2",{},"I am an h2 tag")
-        ]
-    )]
-);
-
-//JSX (to make above work simpler)
-
-const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Hello World using React!");
-
+const jsxHeading = <h1>Namste React!!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent); 
+root.render(jsxHeading);
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 Namaste React Course
+
+From Episode 1(App.js File)
+/**
+ * 
+ *   <div id="parent">
+ *       <div id="child">
+ *           <h2>I am h2 an tag</h2>
+ *           <h1>I am h1 an tag</h1>
+ *       </div>
+ *   </div> 
+ *   <div id="parent">
+ *       <div id="child">
+ *           <h2>I am h2 an tag</h2>
+ *           <h1>I am h1 an tag</h1>
+ *       </div>
+ *   </div>
+*/
+
+const parent = React.createElement("div",{ id: "parent" },
+    [React.createElement("div",{ id: "child" },
+        [
+            React.createElement( "h1",{},"I am an h1 tag"),
+            React.createElement( "h2",{},"I am an h2 tag")
+        ]
+    ),
+    React.createElement("div",{ id: "child" },
+        [
+            React.createElement( "h1",{},"I am an h1 tag"),
+            React.createElement( "h2",{},"I am an h2 tag")
+        ]
+    )]
+);
+
+//JSX is used to make above work simpler 
+
+const heading = React.createElement(
+    "h1",
+    { id: "heading" },
+    "Hello World using React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); 
