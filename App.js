efec108aafffc90424f878db2b910c68c4a0cbe9
ssubudhi <ssubudhi@sashsys.com>
2024-06-19 13:58:43 +0530

    UI Enhancement

--- App.js
+++ App.js
@@ -25,67 +25,23 @@ const Header = () => {
 
 const RestaurantCard = () => {
   return (
-    <div
-      style={{
-        width: "300px",
-        padding: "10px",
-        boxSizing:'border-box'
-      }}
-    >
+    <div className="rest-card">
       <img
         src="https://www.thespruceeats.com/thmb/SalyKjzBU-K1Bv-FTFWnbd6ckjY=/2121x1414/filters:fill(auto,1)/GettyImages-639704020-5c4a63ecc9e77c00017bfebf.jpg"
-        style={{ width: "300px", maxWidth: "100%", height: "auto", borderRadius: "20px"}}
+        className="rest-img"
       />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          margin: "0",
-          padding: "0",
-        }}
-      >
-        <h1
-          className="no-margin-padding"
-          style={{ margin: "1rem 0rem 0.5rem 0rem" }}
-        >
-          Hotel Paradise
-        </h1>
-        <h2
-          style={{
-            display: "flex",
-            alignItems: "center",
-            padding: "0px",
-            margin: "0px 0px 10px 0px",
-          }}
-        >
+      <div className="rest-content">
+        <h1 className="rest-name">Hotel Paradise</h1>
+        <h2 className="rest-rating-time">
           <StarsIcon
             style={{ fontSize: 25, marginRight: "2px", color: "green" }}
           />
           4.5
-          <FiberManualRecordIcon style={{ fontSize: 15, margin: "0px 5px" }} />
+          <FiberManualRecordIcon style={{ fontSize: 10, margin: "0px 5px" }} />
           30-35 mins
         </h2>
-        <p
-          className="no-margin-padding"
-          style={{
-            marginBottom: "5px",
-            fontSize: "1.25rem",
-            color: "#676a6d",
-          }}
-        >
-          Chinese, South Indian, North Indian
-        </p>
-        <p
-          className="no-margin-padding"
-          style={{
-            marginBottom: "5px",
-            fontSize: "1.25rem",
-            color: "#676a6d",
-          }}
-        >
-          Gachibowli, Hyderabad
-        </p>
+        <p className="rest-cuisine">Chinese, South Indian, North Indian</p>
+        <p className="rest-address">Gachibowli, Hyderabad</p>
       </div>
     </div>
   );
@@ -97,7 +53,15 @@ const Body = () => {
       <div className="search-container" style={{ padding: "10px" }}>
         Search
       </div>
-      <div className="restaurant-container" style={{ display: 'flex',flexDirection:'row',flexWrap: 'wrap' }}>
+      <div
+        className="restaurant-container"
+        style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+      >
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
         <RestaurantCard />
         <RestaurantCard />
