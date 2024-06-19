9e63a2f5cf9fe6786162c809c0fa5493c0b0d142
ssubudhi <ssubudhi@sashsys.com>
2024-06-19 13:35:54 +0530

    Episode 4 changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import StarsIcon from "@mui/icons-material/Stars";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 const Header = () => {
   return (
     <div className="header">
@@ -26,22 +27,66 @@ const RestaurantCard = () => {
   return (
     <div
       style={{
-        backgroundColor: "lightgray",
-        width: "fit-content",
+        width: "300px",
         padding: "10px",
+        boxSizing:'border-box'
       }}
     >
       <img
         src="https://www.thespruceeats.com/thmb/SalyKjzBU-K1Bv-FTFWnbd6ckjY=/2121x1414/filters:fill(auto,1)/GettyImages-639704020-5c4a63ecc9e77c00017bfebf.jpg"
-        style={{ width: "200px" }}
+        style={{ width: "300px", maxWidth: "100%", height: "auto", borderRadius: "20px"}}
       />
-      <p>Hotel Paradise</p>
-      <div style={{display:'flex', justifyContent:'flex-start'}}>
-        <p style={{ padding: '10px'}}>*4.5</p>
-        <p style={{padding: '10px'}}>30-35 mins</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          margin: "0",
+          padding: "0",
+        }}
+      >
+        <h1
+          className="no-margin-padding"
+          style={{ margin: "1rem 0rem 0.5rem 0rem" }}
+        >
+          Hotel Paradise
+        </h1>
+        <h2
+          style={{
+            display: "flex",
+            alignItems: "center",
+            padding: "0px",
+            margin: "0px 0px 10px 0px",
+          }}
+        >
+          <StarsIcon
+            style={{ fontSize: 25, marginRight: "2px", color: "green" }}
+          />
+          4.5
+          <FiberManualRecordIcon style={{ fontSize: 15, margin: "0px 5px" }} />
+          30-35 mins
+        </h2>
+        <p
+          className="no-margin-padding"
+          style={{
+            marginBottom: "5px",
+            fontSize: "1.25rem",
+            color: "#676a6d",
+          }}
+        >
+          Chinese, South Indian, North Indian
+        </p>
+        <p
+          className="no-margin-padding"
+          style={{
+            marginBottom: "5px",
+            fontSize: "1.25rem",
+            color: "#676a6d",
+          }}
+        >
+          Gachibowli, Hyderabad
+        </p>
       </div>
-      <h3>Chinese, South Indian, North Indian</h3>
-      <h3>Gachibowli, Hyderabad</h3>
     </div>
   );
 };
@@ -52,7 +97,17 @@ const Body = () => {
       <div className="search-container" style={{ padding: "10px" }}>
         Search
       </div>
-      <div className="restaurant-container">
+      <div className="restaurant-container" style={{ display: 'flex',flexDirection:'row',flexWrap: 'wrap' }}>
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
       </div>
     </div>
