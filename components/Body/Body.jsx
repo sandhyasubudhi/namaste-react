75bcc441f7061574684dce87bfad76b0fcdfcd3e
ssubudhi <ssubudhi@sashsys.com>
2024-06-25 17:43:49 +0530

    Shimmer UI Changes

--- components/Body/Body.jsx
+++ components/Body/Body.jsx
@@ -1,36 +1,58 @@
-
 import { useEffect, useState } from "react";
 import { listOfRestaurant } from "../../utils/restData";
-import RestaurantCard from '../common-component/RestaurantCard'
+import RestaurantCard from "../common-component/RestaurantCard";
+import ShimmerLoader from "../common-component/ShimmerLoader";
 
 const Body = () => {
-  const [restList, setRestList] = useState([])
+  const [restList, setRestList] = useState([]);
   const [restData, setRestData] = useState([]);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
 
   useEffect(async () => {
-    const restaurantData = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
-    const json = await restaurantData.json()
+    const restaurantData = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await restaurantData.json();
     //Optional Chaining
-    setRestList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-    setRestData(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }, [])
+    setRestList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setRestData(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  }, []);
 
-  return restData?.length === 0 ? <div>Loading...</div> : (
+  return (
     <div className="body">
       <div className="search-container" style={{ padding: "10px" }}>
-        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-        <button onClick={() => {
-          const filteredData = restList.filter((data) => data?.info?.name?.toLowerCase()?.includes(search?.toLowerCase()))
-          setRestData(filteredData)
-        }}>Search</button>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            const filteredData = restList.filter((data) =>
+              data?.info?.name?.toLowerCase()?.includes(search?.toLowerCase())
+            );
+            setRestData(filteredData);
+          }}
+        >
+          Search
+        </button>
       </div>
-      <div
-        className="restaurant-container"
-        style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
-      >
-        {restData.map((rest) => (<RestaurantCard key={rest?.info?.id} restData={rest?.info} />))}
-      </div>
+      {restData?.length === 0 ? (
+        <ShimmerLoader />
+      ) : (
+        <div
+          className="rest-container"
+          style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+        >
+          {restData.map((rest) => (
+            <RestaurantCard key={rest?.info?.id} restData={rest?.info} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
---
+++ components/common-component/ShimmerLoader.jsx
@@ -0,0 +1,80 @@
+const ShimmerLoader = () => {
+  return (
+    <div className="loader-cards">
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+      <div className="food-cards">
+        <div>
+          <div className="card-img"></div>
+        </div>
+        <div>
+          <div className="card-info"></div>
+          <div className="card-info"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShimmerLoader;
